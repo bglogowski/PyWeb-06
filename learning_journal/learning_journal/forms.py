ac29d1427480de3c16eafb3ee0e1fdba95970096
Bryan Glogowski <bglogowski@bglogowski-4463.local>
2016-05-11 17:08:23 -0700

    added files for Week 6/7 homework

---
+++ learning_journal/learning_journal/forms.py
@@ -0,0 +1,15 @@
+from wtforms import Form, TextField, TextAreaField, validators, PasswordField
+
+strip_filter = lambda x: x.strip() if x else None
+
+class EntryCreateForm(Form):
+    title = TextField('Entry Title', [validators.Length(min=1, max=255)], filters=[strip_filter])
+    body = TextAreaField('Entry Body', [validators.Length(min=1)], filters=[strip_filter])
+
+class EntryEditForm(EntryCreateForm):
+    pass
+
+class LoginForm(Form):
+    username = TextField('Username', [validators.Length(min=1, max=255)])
+    password = PasswordField('Password', [validators.Length(min=1, max=255)])
+
